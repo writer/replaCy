e72cc960b898cda62fca8ae3bf9287046a8eb9a7
Sam Havens <sam.havens@qordoba.com>
2020-03-02 11:54:13 -0800

    update tests

--- tests/test_custom_props.py
+++ tests/test_custom_props.py
@@ -23,36 +23,45 @@ r_matcher.match_dict.update(
             "yo": "yoyo",
             "whoa": ["it's", "a", "list"],
             "damn": {"a dict": "too?"},
-            "nice": 69,
-            "also_nice": 6.9,
+            "nice": 420,
+            "also_nice": 42.0,
             "meh": True,
         }
     }
 )
 new_matcher = ReplaceMatcher(nlp, r_matcher.match_dict)
+# This matches the new entry above
 matched_span = new_matcher("sometest")[0]
+
+# This matches a "normal" replaCy match example, so uses defaults
 no_match_span = new_matcher("I will extract revenge")[0]
 
 
 def test_custom_properties_string():
     assert no_match_span._.yo == "", "automatically infers string types"
+    assert matched_span._.yo == "yoyo", "picks up custom string types"
 
 
 def test_custom_properties_list():
     assert no_match_span._.whoa == [], "automatically infers list types"
+    assert matched_span._.whoa == ["it's", "a", "list"], "picks up custom list types"
 
 
 def test_custom_properties_dict():
     assert no_match_span._.damn == {}, "automatically infers dict types"
+    assert matched_span._.damn == {"a dict": "too?"}, "picks up custom dict types"
 
 
 def test_custom_properties_int():
     assert no_match_span._.nice == 0, "automatically infers int types"
+    assert matched_span._.nice == 420, "picks up custom int types"
 
 
 def test_custom_properties_float():
     assert no_match_span._.also_nice == 0.0, "automatically infers float types"
+    assert matched_span._.also_nice == 42.0, "picks up custom float types"
 
 
 def test_custom_properties_bool():
     assert no_match_span._.meh == False, "automatically infers bool types"
+    assert matched_span._.meh == True, "picks up custom bool types"
